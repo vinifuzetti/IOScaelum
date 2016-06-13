575c80344220cd5c886d23e1a1a700af96a8f9db
vrfuzetti@gmail.com <vrfuzetti@gmail.com>
2016-06-13 11:48:54 -0300

    Tela basica da aplicacao

---
+++ ContatosIP67/ContatosIP67/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  ContatosIP67
+//
+//  Created by ios6400 on 13/06/16.
+//  Copyright © 2016 Caelum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (IBAction)pegaDadosDoFormulario{
+    NSLog(@"Botao Clicado");
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ContatosIP67/ContatosIP67/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios6400 on 13/06/16.
+//  Copyright © 2016 Caelum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
